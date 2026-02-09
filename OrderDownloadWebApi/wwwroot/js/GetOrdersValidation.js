fc79d07b4aa9c0f010187d05e04ebce5da4dc631
Andrés J. Canillas P. <68328007+AndresCanillas@users.noreply.github.com>
2026-02-09 19:41:40 +0100

    Improve order validations and image download

---
+++ OrderDownloadWebApi/wwwroot/js/GetOrdersValidation.js
@@ -0,0 +1,64 @@
+(function (root, factory) {
+    if (typeof module !== "undefined" && module.exports) {
+        module.exports = factory();
+    } else {
+        root.GetOrdersValidation = factory();
+    }
+})(this, function () {
+    "use strict";
+
+    var ORDER_NUMBER_LENGTH = 10;
+    var VENDOR_ID_MIN_LENGTH = 5;
+    var VENDOR_ID_MAX_LENGTH = 10;
+    var CAMPAIGN_CODE_PATTERN = /^[IVPO][0-9]{2}$/i;
+
+    function normalizeCampaignCode(value) {
+        if (!value) return "";
+        return value.trim().toUpperCase();
+    }
+
+    function isNumeric(value) {
+        return /^[0-9]+$/.test(value);
+    }
+
+    function validateOrderNumber(value) {
+        if (!value) return false;
+        if (!isNumeric(value)) return false;
+        if (value.length !== ORDER_NUMBER_LENGTH) return false;
+        return value[0] !== "0";
+    }
+
+    function validateVendorId(value) {
+        if (!value) return false;
+        if (!isNumeric(value)) return false;
+        return value.length >= VENDOR_ID_MIN_LENGTH && value.length <= VENDOR_ID_MAX_LENGTH;
+    }
+
+    function validateCampaignCode(value) {
+        if (!value) return false;
+        return CAMPAIGN_CODE_PATTERN.test(value.trim());
+    }
+
+    function validateInputs(input) {
+        var normalizedCampaign = normalizeCampaignCode(input.campaignCode || "");
+        return {
+            orderNumberValid: validateOrderNumber(input.orderNumber || ""),
+            vendorIdValid: validateVendorId(input.vendorId || ""),
+            campaignCodeValid: validateCampaignCode(normalizedCampaign),
+            normalizedCampaignCode: normalizedCampaign
+        };
+    }
+
+    return {
+        constants: {
+            ORDER_NUMBER_LENGTH: ORDER_NUMBER_LENGTH,
+            VENDOR_ID_MIN_LENGTH: VENDOR_ID_MIN_LENGTH,
+            VENDOR_ID_MAX_LENGTH: VENDOR_ID_MAX_LENGTH
+        },
+        normalizeCampaignCode: normalizeCampaignCode,
+        validateOrderNumber: validateOrderNumber,
+        validateVendorId: validateVendorId,
+        validateCampaignCode: validateCampaignCode,
+        validateInputs: validateInputs
+    };
+});
